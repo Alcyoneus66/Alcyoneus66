3bddcca6edd7fd43240f6af11383d283501e6986
Alcyoneus66 <105869856+Alcyoneus66@users.noreply.github.com>
2022-06-23 22:00:32 +0800

    Update IP_API.js

--- QuantumultX/IP_API.js
+++ QuantumultX/IP_API.js
@@ -42,6 +42,7 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ obj['country'];
-var subtitle =  "💋 "+ ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
+var subtitle ='🐉'+City_ValidCheck(obj['city'])+'-'+'('+ ISP_ValidCheck(obj['org'])+')'+'🐉';
+var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
